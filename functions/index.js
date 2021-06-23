8141347276df1b68a79da4d35471756ee335431c
Ari Wilson <ari.wilson@gmail.com>
2021-06-22 21:46:46 -0700

    initial commits

--- functions/index.js
+++ functions/index.js
@@ -7,3 +7,20 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.startRound = functions.https.onRequest((request, response) => {
+  firebase.database().ref('games/' +gameId).set({
+    num_players: 4
+    current_double: 9
+  })
+});
+
+exports.takeTurn = functions.https.onRequest((request, response) => {
+  if (request.pieceSelected !== null and request.lineSelected !== null) {
+    // place a piece
+
+  } else {
+    // need to draw a piece
+  }
+});
+
