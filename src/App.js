ace84cf0164253157c9061ac02a35cb29e6fc196
Ari Wilson <ari.wilson@gmail.com>
2021-06-24 15:07:21 -0700

    npm start and firebase serve / run emulators work fine now. also added some basic sections and css styling

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
 import firebase from 'firebase/app';
-import 'firebase/
+import 'firebase/firestore';
+import 'firebase/functions';
+
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+import { firebaseConfig } from './firebaseConfig'
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
+
+const firestore = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h2>Joyce Dominoes</h2>
       </header>
+      <section className="GameInfo">
+        <GameInfo />
+      </section>
+      <section className="Playfield">
+        <Playfield />
+      </section>
+      <section className="Hand">
+        <Hand />
+      </section>
     </div>
   );
 }
+
+function GameInfo() {
+  return (<p>GameInfo</p>);
+}
+
+function Playfield() {
+  return (<p>Playfield</p>);
+}
+
+function Hand() {
+  return (<p>Hand</p>);
+}
+
 
 export default App;
