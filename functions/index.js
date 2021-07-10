d26d7681ee1c05072807341c4a34e1860bf64b02
Ari Wilson <ari.wilson@gmail.com>
2021-07-10 12:09:06 -0700

    add walking/next turn actions

--- functions/index.js
+++ functions/index.js
@@ -45,6 +45,14 @@ function isDoubleFun(currentDouble) {
   return (tile) => tile.end1 === currentDouble && tile.end2 === currentDouble;
 }
 
+const actions = {
+  NONE: 0,
+  PLAY: 1,
+  DRAW: 2,
+  PASS: 3,
+  WALKING: 4,
+}
+
 exports.startGame = functions.https.onCall((data, context) => {
   // double 9 set has 55 tiles - (n+1)(n=2)/2
   const tiles = Array(55);
@@ -63,7 +71,8 @@ exports.startGame = functions.https.onCall((data, context) => {
       name: "Player " + playerNumber,
       score: 0,
       hand: shuffled_tiles.slice(tile_index, tile_index + tiles_per_player),
-      penny: false
+      penny: false,
+      walking: false,
     };
   }
   const game = {
@@ -86,6 +95,7 @@ exports.startGame = functions.https.onCall((data, context) => {
               game.currentDouble, game.currentDouble)];
           game.players[j].hand.splice(doubleIndex, 1);
           game.currentPlayer = game.players[j].name;
+          // game.currentActions = [actions.NONE];
           game.unusedDoubles.splice(i, 1);
           foundDouble = true;
           break;
@@ -102,8 +112,7 @@ exports.startGame = functions.https.onCall((data, context) => {
   admin.database().ref(`game/${data.gameId}`).set(game);
 });
 
-
-exports.startRound = functions.https.onRequest(async (request, response) => {
+exports.startRound = functions.https.onCall((data, context) => {
   // const writeResult = await admin.firestore().collection("games").add({
   //   numPlayers: 4,
   //   currentDouble: 9,
@@ -112,12 +121,39 @@ exports.startRound = functions.https.onRequest(async (request, response) => {
   });
 });
 
-exports.takeTurn = functions.https.onRequest((request, response) => {
-  if (request.pieceSelected !== null && request.lineSelected !== null) {
-    // place a piece
-  } else {
-    // need to draw a piece
-  }
-  response.send("hello from firebase");
+exports.takeAction = functions.https.onCall((data, context) => {
+  admin.database().ref(`game/${data.gameId}`).get().then((snapshot) => {
+    var game = snapshot.val();
+    switch (data.action) {
+      case actions.PLAY:
+        // play(game, data.tile, data.line);
+        break;
+      case actions.DRAW:
+        // draw(game);
+        break;
+      case actions.PASS:
+        for (let i = 0; i < game.players.length; i++) {
+          if (game.players[i].name == game.currentPlayer) {
+            const nextPlayer = (i + 1) % game.players.length + 1
+            game.currentPlayer = `Player ${nextPlayer}`;
+            break;
+          }
+        }
+        // pass(game);
+        break;
+      case actions.WALKING:
+        for (let i = 0; i < game.players.length; i++) {
+          if (game.players[i].name == game.currentPlayer) {
+            game.players[i].walking = true;
+            break;
+          }
+        }
+        break;
+      default:
+        throw new functions.https.HttpsError(
+            'invalid-argument', 'Invalid action specified.');
+    }
+    admin.database().ref(`game/${data.gameId}`).set(game);
+  });
 });
 
